8b827cc43842fbf4ef0fe41b255a7b62d0f98cfa
Shai07 <94008671+Shai07@users.noreply.github.com>
2025-06-23 11:44:48 -0400

    used the toFixed method to round to nearest tenth

--- 11_tempConversion/tempConversion.js
+++ 11_tempConversion/tempConversion.js
@@ -1,7 +1,9 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fahrenheitTemp) {
+  return Number(((fahrenheitTemp-32)*(5/9)).toFixed(1));
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celsiusTemp) {
+  return Number(((celsiusTemp*1.8)+32).toFixed(1));
 };
 
 // Do not edit below this line
