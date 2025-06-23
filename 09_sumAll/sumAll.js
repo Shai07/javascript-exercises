ff8fbf65c35ef5fcfdce464f6bc12b28320057a8
Shai07 <94008671+Shai07@users.noreply.github.com>
2025-06-23 11:29:25 -0400

    used typeof to check it's a number and fixed ? i could have used !Number.isInteger(number) to condense if statement as it would check it is a number type and an integer.

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,10 +1,13 @@
 const sumAll = function(number1, number2) {
-    if(number1 < 0 || number2 < 0 || isNaN(number1) || isNaN(number2) || number1%1 !== 0 || number2%1 !== 0) return "ERROR";
+    if(number1 < 0 || number2 < 0 || 
+        typeof number1 !== 'number' || typeof number2 !== 'number'
+         || number1%1 !== 0 || number2%1 !== 0) 
+        return "ERROR";
     let sum = 0;
-    let startingNumber = a ? a < b:b;
-    let endingNumber = b ? a > b:a;
-    for (let i = 0; i < endingNumber; i++) {
-        sum += i + startingNumber;
+    let startingNumber = (number1 < number2) ? number1:number2;
+    let endingNumber = (number1 > number2) ?  number1:number2;
+    for (let i = startingNumber; i <= endingNumber; i++) {
+        sum += i;
     }
     return sum;
 };
