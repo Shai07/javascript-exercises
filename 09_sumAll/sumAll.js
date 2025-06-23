7617aa28cd53bfd092c64e0d752a76bd055e3c56
Shai07 <94008671+Shai07@users.noreply.github.com>
2025-06-23 01:27:42 -0400

    didn't complete exercise. Need to figure out ? var I need to research how to use the ? variable assignment

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,5 +1,12 @@
-const sumAll = function() {
-
+const sumAll = function(number1, number2) {
+    if(number1 < 0 || number2 < 0 || isNaN(number1) || isNaN(number2) || number1%1 !== 0 || number2%1 !== 0) return "ERROR";
+    let sum = 0;
+    let startingNumber = a ? a < b:b;
+    let endingNumber = b ? a > b:a;
+    for (let i = 0; i < endingNumber; i++) {
+        sum += i + startingNumber;
+    }
+    return sum;
 };
 
 // Do not edit below this line
