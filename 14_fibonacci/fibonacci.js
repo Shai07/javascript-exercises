139a67c730430db2bf1903f566993349621d4fb4
Shai07 <94008671+Shai07@users.noreply.github.com>
2025-07-09 10:54:54 -0400

    created fibonacci array using for loop to get index

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,4 +1,10 @@
-const fibonacci = function() {
+const fibonacci = function(index) {
+    let fibonacci = [0, 1];
+    if (index < 0) return 'OOPS';
+    for (let i = 1; i < index; i++) {
+        fibonacci.push(fibonacci[fibonacci.length-1]+fibonacci[fibonacci.length-2]);
+    }
+    return fibonacci[index];
 
 };
 
