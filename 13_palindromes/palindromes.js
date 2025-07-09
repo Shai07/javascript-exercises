1e15c2081246eb0f222fd0fe8a5af51dc393a582
Shai07 <94008671+Shai07@users.noreply.github.com>
2025-07-08 22:10:43 -0400

    checked if palindromes by cleaning word of special and comparing to reversed

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,5 +1,8 @@
-const palindromes = function () {
-
+const palindromes = function (word) {
+    let specialChar = /^[!@#$%^&*()_+\-=\[\]{};' :"\\|,.<>\/?]*$/;
+    let cleanWord = word.toLowerCase().split('').filter((letter) => (!letter.match(specialChar))).join('');
+    console.log(cleanWord);
+    return cleanWord === cleanWord.split('').reverse().join('');    
 };
 
 // Do not edit below this line
