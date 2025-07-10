f6682e394f65a62fe53d51264856232f90654be1
Shai07 <94008671+Shai07@users.noreply.github.com>
2025-07-09 23:03:03 -0400

    used map and 'key' in obj to check age

--- 16_findTheOldest/findTheOldest.js
+++ 16_findTheOldest/findTheOldest.js
@@ -1,4 +1,15 @@
-const findTheOldest = function() {
+const findTheOldest = function(array) {
+    let agesArray = [];
+    const present = new Date().getFullYear();
+    agesArray = array.map((person) => {
+        if (!('yearOfDeath' in person)) {
+            return present - person.yearOfBirth;
+        }
+        else {
+            return person.yearOfDeath - person.yearOfBirth
+        }
+    });
+    return array[agesArray.indexOf(Math.max(...agesArray))];
 
 };
 
